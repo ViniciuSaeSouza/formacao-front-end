8395cc3bbb59205a631ce19982337e2e598cef22
Vinicius Saes <viniciusaesouza@gmail.com>
2024-02-08 16:31:23 -0300

    Update scripts.js

--- JavaScript/2_operadores/js/scripts.js
+++ JavaScript/2_operadores/js/scripts.js
@@ -10,14 +10,86 @@ console.log(5 * 4);
 console.log(10 / 2);
 
 // 3 - Special Numbers
-console.log (typeof Infinity); // Infinity com 'i' Maiúsculo
+console.log(typeof Infinity); // Infinity com 'i' Maiúsculo
 
 console.log(typeof -Infinity);
 
-console.log(typeof 12 * 'asd');
+console.log(typeof 12 * "asd");
 
-console.log(typeof NaN)
+console.log(typeof NaN);
 
 // 4 - Strings
-console.log("Um texto");
+console.log(typeof "Um texto em aspas");
+
+console.log(typeof "Um texto em aspas simples");
+
+console.log(typeof `Um texto em cràse`);
+
+// 5 - Simbolos especiais em strings
+console.log("Testando a \n quebra de linha");
+
+console.log("Espaçamento \t de tab");
+
+// 6 - Concatenação
+console.log("Oi," + " tudo" + " bem?");
+
+console.log(`Testando` + ` com` + ` crase`);
+
+// 7 - Template Strings
+console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+
+// 8 - Boolean
+console.log(true);
+
+console.log(false);
+
+console.log(5 > 20);
+
+console.log(30 > 10);
+
+console.log(typeof true);
+
+// 9 - Comparações
+console.log(5 <= 5);
+
+console.log(5 < 5);
+
+console.log(10 == 10);
+
+console.log(10 == 9);
+
+console.log(10 != 9);
+
+// 10 - Idêntico
+console.log(9 == "9");
+
+console.log(9 === "9");
+
+console.log(9 != "9");
+
+console.log(9 !== "9");
+
+// 11 - Operadores Lógicos
+console.log(true && true);
+
+console.log(true && false);
+
+console.log(5 > 2 && 2 < 10);
+
+console.log(5 > 2 && "Matheus" === 1);
+
+console.log(5 > 2 || "Matheus" === 1);
+
+console.log(5 < 2 || 2 > 10);
+
+console.log(!true);
+
+console.log(!5 > 2);
+
+// 12 - Empty Values 
+console.log(typeof null, typeof undefined);
+
+console.log(null == undefined)
+
+console.log(null === undefined)
 
