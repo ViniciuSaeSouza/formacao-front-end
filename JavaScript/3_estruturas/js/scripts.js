a18c5ef824216126acd421df5b4e5a3883b72fdf
Vinicius Saes <viniciusaesouza@gmail.com>
2024-02-15 23:35:57 -0300

    Update scripts.js

--- JavaScript/3_estruturas/js/scripts.js
+++ JavaScript/3_estruturas/js/scripts.js
@@ -1,2 +1,36 @@
 // 1 - variáveis
 let nome = "Saes";
+
+console.log(nome);
+
+nome = "Saes Souza";
+
+console.log(nome);
+
+const sexo = "Homem"; //const = constante, não muda seu valor.
+
+console.log(sexo);
+
+// 2 - mais sobre variáveis
+// let 2test = "inválido";
+// let @test = "inválido";
+
+let a = 10,
+  b = 20,
+  c = 30;
+
+console.log(a, b, c);
+
+const nomecompleto = "Matheus Battisti";
+
+const nomeCompleto = "João da Silva";
+
+console.log(nomecompleto);
+
+console.log(nomeCompleto);
+
+let _test = "válido";
+
+let $test = "válido";
+
+console.log(_test, $test)
